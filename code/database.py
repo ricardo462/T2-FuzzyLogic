e12f165dc4f1c85cf1d1618b26b3fe33b0489091
ricardo <ricardo.rg462@gmail.com>
2022-05-23 20:39:34 -0400

    creating a database

--- code/database.py
+++ code/database.py
@@ -1,1 +1,19 @@
+from distutils.util import execute
 import sqlite3
+
+connection = sqlite3.connect('competitors.db')
+try:
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE competitors (name text, basckets integer, height, time real)")
+except:
+    print('No se pudo crear la base de datos')
+
+
+personas = [('ric', 15, 183, 10), ('seb', 20, 182, 10)]
+cursor.executemany('INSERT INTO competitors values (?, ?, ?, ?)', personas)
+
+
+for row in cursor.execute('SELECT * FROM competitors'):
+    print(row)
+
+connection.close()
