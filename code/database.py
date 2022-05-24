d1a95d740f798a18b1fb2d8ee4a4e7fbdfbb7c9e
Ricardo <ricardo.rg462@gmail.com>
2022-05-24 17:07:50 -0400

    Final code

--- code/database.py
+++ code/database.py
@@ -6,8 +6,8 @@ cursor = connection.cursor()
 
 try:
     cursor.execute("CREATE TABLE competitors (name text, baskets integer, height, time real, score real)")
-except:
-    print('No se pudo crear la base de datos')
+except Exception as e:
+    print(e)
 
 connection.commit()
 connection.close()
